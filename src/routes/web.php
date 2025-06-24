20e6003f8297cf61412bb59a6b7a927769f288b0
Robius Sani <sani15-257@diu.edu.bd>
2025-06-24 16:49:38 +0600

    feat: included redis support #5

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redis-test', function () {
+    Redis::set('test-key', 'Redis is working!');
+    return Redis::get('test-key');
 });
