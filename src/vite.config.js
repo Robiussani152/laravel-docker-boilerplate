bd89ac646f2044bbabd9fb2190ed84a7ceed7b59
Robius Sani <sani15-257@diu.edu.bd>
2025-06-24 15:08:44 +0600

    feat: vite config updated to work with docker #3

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
+    server: {
+        https: {
+            key: fs.readFileSync("./storage/certs/laravel-docker.test-key.pem"),
+            cert: fs.readFileSync("./storage/certs/laravel-docker.test.pem"),
+        },
+        host: "0.0.0.0",
+        port: 5173,
+        hmr: {
+            host: process.env.APP_DOMAIN,
+        },
+        strictPort: true,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
